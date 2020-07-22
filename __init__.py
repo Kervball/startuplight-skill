dc4cd92a9e66cd0f685fe4f9534c423342d39698
James <jameskervin.2012@gmail.com>
2020-07-22 19:52:14 -0400

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -41,17 +41,13 @@ class Startuplight(MycroftSkill):
 #this function causes the lights to fade teal when ready to go
     def handler_mycroft_ready(self, message):
         for brightness in range(0,100):
-            wiringpi.softPwmWrite(blue_back, brightness)
-            wiringpi.softPwmWrite(blue_front, brightness)
-            wiringpi.softPwmWrite(green_back, brightness)
-            wiringpi.softPwmWrite(green_front, brightness)
-            wiringpi.delay(10)
+            wiringpi.softPwmWrite(red_back, brightness)
+            wiringpi.softPwmWrite(red_front, brightness)
+            wiringpi.delay(5)
         for brightness in reversed(range(0,100)):
-            wiringpi.softPwmWrite(blue_back, brightness)
-            wiringpi.softPwmWrite(blue_front, brightness)
-            wiringpi.softPwmWrite(green_back, brightness)
-            wiringpi.softPwmWrite(green_front, brightness)
-            wiringpi.delay(10)
+            wiringpi.softPwmWrite(red_back, brightness)
+            wiringpi.softPwmWrite(red_front, brightness)
+            wiringpi.delay(5)
 
 #this code fades white when the code word is reconginzed
     """def handler_wakeword(self, message):
